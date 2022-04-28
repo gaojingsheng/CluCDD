295447580c8675c89cfa165ed520654821e7289d
gaojingsheng <gaojingsheng@sjtu.edu.cn>
2022-04-28 20:59:51 +0800

    Basic code transfer

---
+++ CDCplot.py
@@ -0,0 +1,291 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import AutoModel, AutoTokenizer
+import numpy as np
+import os
+import random
+import logging
+import utils
+from utils import calculate_purity_scores, calculate_shen_f_score
+from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn.cluster import KMeans
+from tqdm import tqdm
+import copy
+import argparse
+import json
+
+from matplotlib import pyplot as plt
+from sklearn.manifold import TSNE
+
+
+
+class RelationModel(nn.Module):
+    def __init__(self, bert_model, hidden_size=768, n_layers=1, uselstm=True, bidirectional=False, freeze_bert=False, dropout=0):
+        super(RelationModel, self).__init__()
+        
+        self.bert = bert_model
+        self.lstm = nn.LSTM(hidden_size, hidden_size, n_layers, batch_first=True, \
+                                        bidirectional=bidirectional, dropout=dropout)
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.uselstm = uselstm
+        self.linear = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size), 
+            nn.ReLU()
+        )
+        # self.bn = nn.BatchNorm1d(hidden_size, affine=False)
+        
+        self.freeze_parameters(self.bert)
+
+    def freeze_parameters(self,model):
+        for name, param in model.named_parameters():  
+            param.requires_grad = False
+            if "encoder.layer.11" in name or "pooler" in name:
+                param.requires_grad = True
+    
+    def attention_score(self, input_feats, T=0.5):
+        # input_feat: N*C*768
+        new_feats = input_feats.transpose(1,2)
+        matrix = torch.exp(torch.matmul(input_feats, new_feats)*T)
+        new_matrix = torch.zeros_like(matrix).cuda()
+        for i in range(input_feats.size(0)):
+            for j in range(input_feats.size(1)):
+                new_matrix[i][j] = matrix[i][j]/torch.sum(matrix[i][j])
+        return new_matrix
+
+    def forward(self, inputs, mask):
+            
+        # if self.args.mean_pooling:
+        encoded_layer_12 = self.bert(**inputs, output_hidden_states=True, return_dict=True).last_hidden_state
+        embeddings = self.dense(encoded_layer_12.mean(dim = 1))
+        # else:        
+        #     # pooled_output = self.bert(**inputs, return_dict=True).pooler_output
+        #     embeddings = self.bert(**inputs, return_dict=True).last_hidden_state[0]
+
+        conversation_feats = embeddings.view(mask.size(0), -1, embeddings.size(-1)) 
+
+        conversation_feats, _ = self.lstm(conversation_feats)
+
+        linear_feats = self.linear(conversation_feats)
+
+        # linear_feats = linear_feats*mask.unsqueeze(2)
+        norm_feats = F.normalize(linear_feats, p=2, dim=2)
+        return linear_feats
+        
+
+class TrainDataLoader(object):
+    def __init__(self, args, all_utterances, labels, name='train'):
+        self.batch_size = args.batch_size
+
+        self.all_utterances_batch = [all_utterances[i:i+self.batch_size] \
+                                    for i in range(0, len(all_utterances), self.batch_size)]
+        self.labels_batch = [labels[i:i+self.batch_size] \
+                            for i in range(0, len(labels), self.batch_size)]
+
+        print("labels_batch",len(self.labels_batch))
+        assert len(self.all_utterances_batch) == len(self.labels_batch)
+                               
+        self.batch_num = len(self.all_utterances_batch)
+        print("{} batches created in {} set.".format(self.batch_num, name))
+        
+        self.padding_sentence = "This is a padding sentence: bala bala bala123!"
+    
+    def __len__(self):
+        return self.batch_num
+
+    def padding_conversation(self, utterances, labels):
+        conversations_length = [len(x) for x in utterances]
+        max_conversation_length = max(conversations_length)
+        padded_conversations = []
+        for i in range(len(conversations_length)):
+            for j in range(len(utterances[i])):
+                if len(utterances[i][j]) > 600:
+                    utterances[i][j] = utterances[i][j][:600]
+            temp = utterances[i] + [self.padding_sentence]*(max_conversation_length-conversations_length[i])
+            padded_conversations.extend(temp)
+        return padded_conversations, conversations_length, max_conversation_length
+
+    def __getitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError
+        if key < 0 or key >= self.batch_num:
+            raise IndexError
+
+        utterances = self.all_utterances_batch[key]
+        labels = self.labels_batch[key]
+        padded_conversations, conversations_length, max_conversation_length = self.padding_conversation(utterances, labels)
+        mask = torch.arange(max_conversation_length).expand(len(conversations_length), max_conversation_length) \
+                                < torch.Tensor(conversations_length).unsqueeze(1)
+        # padded_conversations: NC * sentences
+        return padded_conversations, labels, mask
+
+
+class ContrastiveLoss(torch.nn.Module):
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1.unsqueeze(0), output2.unsqueeze(0))
+ 
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        return loss_contrastive
+
+class Trainer(object):
+    def __init__(self, args, model, logger, all_utterances, loss_fn=ContrastiveLoss()):
+        self.args = args
+        self.model = model
+        self.logger = logger
+        self.loss_fn = loss_fn
+        self.utterances = all_utterances
+        params = list(self.model.parameters())
+        self.optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.num_labels = args.num_labels
+        self.epoch_num = args.epoch_num
+
+    def evaluate(self, test_loader, epoch=0, mode="dev", first_time = False, plot = False):
+        predicted_labels = []
+        truth_labels = []
+        self.model.eval()
+        with torch.no_grad():
+            for batch_id, batch in enumerate(tqdm(test_loader)):
+                padded_conversations, labels, mask = batch
+                inputs = self.args.tokenizer(padded_conversations, padding=True, truncation=True, return_tensors="pt")
+                for things in inputs:
+                    inputs[things] = inputs[things][:,:self.args.crop_num].cuda()
+                mask = mask.cuda()
+                if first_time:
+                    embeddings = self.model.bert(**inputs, return_dict=True).pooler_output
+                    conversation_feats = embeddings.view(mask.size(0), -1, embeddings.size(-1)) 
+                    linear_feats = conversation_feats*mask.unsqueeze(2)
+                    output_feats = F.normalize(linear_feats, p=2, dim=2)
+                    # sim_matrix = self.model.attention_score(norm_feats, T=0.5)
+                    # output = torch.matmul(sim_matrix, norm_feats)
+                    # output_feats = torch.cat((norm_feats, output), dim=-1)
+                else:
+                    output_feats = self.model(inputs, mask)
+
+                for i in range(len(labels)):
+                    batch_con_length = torch.sum(mask[i])
+                    km = KMeans(n_clusters = self.num_labels).fit(output_feats[i][:batch_con_length].detach().cpu().numpy())
+                    pseudo_labels =  km.labels_
+                    predicted_labels.append(pseudo_labels)
+                    truth_labels.append(labels[i])
+                    """
+                    if plot and batch_id < 3:
+                        fig = plt.figure()
+                        ax = plt.subplot(111)
+                        x = output_feats[i][:batch_con_length].detach().cpu().numpy()
+                        x_embedded = TSNE(n_components=2).fit_transform(x)
+                        plt.scatter(x_embedded[:,0], x_embedded[:,1], c=labels[i])
+                        fig.savefig('./IJCAI/Plot/epoch{}_dialogue_{}_{}.png'.format(epoch, batch_id, i))
+                        plt.close(fig)"""
+
+        purity_score = utils.compare(predicted_labels, truth_labels, 'purity')
+        nmi_score = utils.compare(predicted_labels, truth_labels, 'NMI')
+        ari_score = utils.compare(predicted_labels, truth_labels, 'ARI')
+        shen_f_score = utils.compare(predicted_labels, truth_labels, 'shen_f')
+        loc3_score = utils.compare(predicted_labels, truth_labels, 'Loc3')
+        log_msg = "purity_score: {}, nmi_score: {}, ari_score: {}, shen_f_score: {}, loc3_score: {}".format(
+                        round(purity_score, 4), round(nmi_score, 4), round(ari_score, 4), round(shen_f_score, 4), round(loc3_score, 4))
+        self.logger.info(log_msg)
+        return
+
+    def train(self, train_loader, dev_loader):
+        step_cnt = 0 
+        # self.evaluate(dev_loader, first_time=True)
+        # self.evaluate(dev_loader, plot=True)
+        # train_loader = self.evaluate(train_loader, mode="train", first_time=True)
+        self.model.train()
+        for epoch in tqdm(range(self.epoch_num)):
+            epoch_loss = 0
+            for i, batch in enumerate(tqdm(train_loader)):
+                step_cnt += 1
+                padded_conversations, labels, mask = batch
+                inputs = self.args.tokenizer(padded_conversations, padding=True, truncation=True, return_tensors="pt")
+                for things in inputs:
+                    inputs[things] = inputs[things][:,:self.args.crop_num].cuda()
+                mask = mask.cuda()
+                output = self.model(inputs, mask)
+                loss = torch.zeros(1, dtype=torch.float32).to(self.device)
+                for batch in range(len(labels)):
+                    train_labels = labels[batch]
+                    for m in range(len(train_labels)-1):
+                        for n in range(m+1, len(train_labels)):
+                            loss += self.loss_fn(output[batch][m], output[batch][n], train_labels[m]!=train_labels[n])
+                loss = loss/((len(train_labels)-1)**2*self.args.batch_size)
+                log_msg = "Epoch : {}, batch: {}/{}, step: {},loss: {}".format(epoch, i, len(train_loader), step_cnt, round(loss.data.item(), 4))
+                self.logger.info(log_msg)
+                epoch_loss += loss.data.item()
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                if (step_cnt % 100 == 0 and epoch == 0) or step_cnt % 1000 == 0:
+                    self.evaluate(dev_loader, first_time=True)
+                    self.evaluate(dev_loader, epoch=epoch+1, plot=False)
+                    self.model.train()
+            log_msg = "Epoch average loss is: {}".format(round(epoch_loss/len(train_loader), 4))
+            self.logger.info(log_msg)
+            model_name = os.path.join("IJCAI/SubModel", "model{}_epoch{}".format(self.args.num_labels, epoch))
+            log_msg = "Saving model at '{}'".format( model_name)
+            self.logger.info(log_msg)
+            torch.save(self.model.state_dict(), model_name)
+            
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_labels', type=int, default=4)
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--epoch_num', type=int, default=10)
+    parser.add_argument('--crop_num', type=int, default=60)
+    parser.add_argument('--learning_rate', type=float, default=5e-4)
+
+    parser.add_argument('--berttype', type=str, default='bert', help="bert, simcse")
+    
+    parser.add_argument('--dataset', type=str, default='dialogue')
+
+    parser.add_argument('--device', type=str, default='0')
+
+    args = parser.parse_args() 
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.device
+    SEED = random.randint(1,100)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    savename = "DCC-Net_"
+
+    all_utterances, labels = utils.readdata(datapath="dataset/splited/entangled{}_train.json".format(args.num_labels), mode='train')
+    dev_utterances, dev_labels = utils.readdata(datapath="dataset/splited/entangled{}_dev.json".format(args.num_labels), mode='dev')
+
+
+    if args.berttype == "simcse":
+        savename += "_SimCSE"
+        args.tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
+        bert_model = AutoModel.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
+    elif args.berttype == "bert":
+        savename += "_Bert"
+        args.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        bert_model = AutoModel.from_pretrained("bert-base-uncased")
+    else:
+        raise ValueError("Input bert type must be bert or simcse.")
+    
+    train_loader = TrainDataLoader(args, all_utterances, labels)
+    dev_loader = TrainDataLoader(args, dev_utterances, dev_labels, name="dev")
+    
+    savename += str(args.num_labels)
+
+    logger_name = os.path.join("./IJCAI/SessionLog", "{}.txt".format(savename))
+    LOG_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+    logging.basicConfig(format=LOG_FORMAT, level=logging.INFO, filename=logger_name, filemode='w')
+    logger = logging.getLogger()
+    
+    log_head = "Learning Rate: {}; Random Seed: {}; Clustering Number: {}; ".format(args.learning_rate, SEED, args.num_labels)
+    logger.info(log_head)
+
+    model = RelationModel(bert_model, hidden_size=768).cuda()
+    trainer = Trainer(args, model, logger, all_utterances)
+
+    trainer.train(train_loader, dev_loader)
---
+++ utils.py
@@ -0,0 +1,274 @@
+import torch
+import json
+import os
+import sys
+import numpy as np 
+import pickle
+from collections import Counter
+from sklearn import metrics
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from tqdm import tqdm
+from ortools.graph import pywrapgraph
+
+def readdata(datapath, mode = "train"):
+    print("Tokenizing sentence and word...")
+    with open(datapath) as fin:
+        content = json.load(fin)
+    print("{} {} data examples read.".format(len(content), mode))
+    all_utterances = []
+    labels = []
+    for item in tqdm(content):
+        utterance_list = []
+        label_list = []
+        for one_uttr in item:
+            uttr_content = one_uttr['utterance']
+            label = one_uttr['label']
+            label_list.append(label)
+            utterance_list.append(uttr_content)
+        all_utterances.append(utterance_list)
+        labels.append(label_list)
+    return all_utterances, labels
+
+def readdata2(datapath, mode = "train"):
+    print("Tokenizing sentence and word...")
+    with open(datapath) as fin:
+        content = json.load(fin)
+    print("{} {} data examples read.".format(len(content), mode))
+    all_utterances = []
+    labels = []
+    speakers = []
+    for item in tqdm(content):
+        utterance_list = []
+        label_list = []
+        speaker_list = []
+        for one_uttr in item:
+            uttr_content = one_uttr['utterance']
+            label = one_uttr['label']
+            speaker = one_uttr['speaker']
+            label_list.append(label)
+            speaker_list.append(speaker)
+            utterance_list.append(uttr_content)
+        all_utterances.append(utterance_list)
+        labels.append(label_list)
+        speakers.append(speaker_list)
+    return all_utterances, labels, speakers
+
+def calculate_purity_scores(y_true, y_pred):
+    # compute contingency matrix (also called confusion matrix)
+    contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
+    # return purity
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix) 
+
+
+def calculate_shen_f_score(y_true, y_pred):
+    def get_f_score(i, j, n_i_j, n_i, n_j):
+        recall = n_i_j / n_i
+        precision = n_i_j / n_j
+        if recall == 0 and precision == 0:
+            f_score = 0.
+        else:
+            f_score = 2 * recall * precision / (recall + precision)
+        return f_score
+    
+    y_true_cnt = dict(Counter(y_true))
+    y_pred_cnt = dict(Counter(y_pred))
+    y_pred_dict = dict()
+    for i, val in enumerate(y_pred):
+        if y_pred_dict.get(val, None) == None:
+            y_pred_dict[val] = dict()
+        if y_pred_dict[val].get(y_true[i], None) == None:
+            y_pred_dict[val][y_true[i]] = 0
+        y_pred_dict[val][y_true[i]] += 1
+    shen_f_score = 0.
+    for i, val_i in y_true_cnt.items():
+        f_list = []
+        for j, val_j in y_pred_cnt.items():
+            f_list.append(get_f_score(i, j, y_pred_dict[j].get(i, 0), val_i, val_j))
+        shen_f_score += max(f_list) * y_true_cnt[i] / len(y_true)
+    return shen_f_score
+
+def elsner_local_error(y_true, y_pred, window=3):# loc3
+    match = 0
+    total = 0
+    anums, gnums = {},{}
+    for i in range(len(y_true)):
+        anums[i] = y_true[i]
+        gnums[i] = y_pred[i]
+        start = 0
+        end = max(len(y_true), len(y_pred)) + 1
+    for num in range(start,end):
+        if num in anums and num in gnums:
+            for i in range(-window, 0):
+                pos = num + i
+                if pos in anums and pos in gnums:
+                    if (gnums[pos] == gnums[num]) == (anums[pos] == anums[num]):
+                        match += 1
+                    total += 1
+    return match / total
+
+def clusters_to_contingency(y_true, y_pred):
+    # A table, in the form of:
+    # https://en.wikipedia.org/wiki/Rand_index#The_contingency_table
+    max_x = max(y_true)+1
+    max_y = max(y_pred)+1
+    table = np.zeros((max_x, max_y), dtype=int)
+    # print(table)
+
+    for i in range(len(y_true)):
+        # print(y_pred[i])
+        table[y_true[i]][y_pred[i]] = table[y_true[i]][y_pred[i]]+1
+        # print(table)
+    print(table)
+    counts_a, counts_g = [], []
+    for i in range(max_x):
+        counts_a.append(sum(table[i]))
+
+    for j in range(max_y):
+        one_j = 0
+        for i in range(max_x):
+            one_j += table[i][j]
+        counts_g.append(one_j)
+    return table.tolist(), counts_a, counts_g
+
+def one_to_one(y_true, y_pred):
+    contingency, row_sums, col_sums = clusters_to_contingency(y_true, y_pred)
+    row_to_num = {}
+    col_to_num = {}
+    num_to_row = []
+    num_to_col = []
+    for row_num, row in enumerate(row_sums):
+        row_to_num[row] = row_num
+        num_to_row.append(row)
+    for col_num, col in enumerate(col_sums):
+        col_to_num[col] = col_num
+        num_to_col.append(col)
+
+    min_cost_flow = pywrapgraph.SimpleMinCostFlow()
+    start_nodes = []
+    end_nodes = []
+    capacities = []
+    costs = []
+    source = len(num_to_row) + len(num_to_col)
+    sink = len(num_to_row) + len(num_to_col) + 1
+    supplies = []
+    tasks = min(len(num_to_row), len(num_to_col))
+    for row, row_num in row_to_num.items():
+        start_nodes.append(source)
+        end_nodes.append(row_num)
+        capacities.append(1)
+        costs.append(0)
+        supplies.append(0)
+    for col, col_num in col_to_num.items():
+        start_nodes.append(col_num + len(num_to_row))
+        end_nodes.append(sink)
+        capacities.append(1)
+        costs.append(0)
+        supplies.append(0)
+    supplies.append(tasks)
+    supplies.append(-tasks)
+    for row, row_num in row_to_num.items():
+        for col, col_num in col_to_num.items():
+            cost = 0
+            print(row, row_num)
+            if col in contingency[row_num]:
+                cost = - contingency[row_num][col_num]
+            start_nodes.append(row_num)
+            end_nodes.append(col_num + len(num_to_row))
+            capacities.append(1)
+            costs.append(cost)
+
+    # Add each arc.
+    for i in range(len(start_nodes)):
+        min_cost_flow.AddArcWithCapacityAndUnitCost(start_nodes[i], end_nodes[i],
+                                                    capacities[i], costs[i])
+  
+    # Add node supplies.
+    for i in range(len(supplies)):
+        min_cost_flow.SetNodeSupply(i, supplies[i])
+
+    # Find the minimum cost flow.
+    min_cost_flow.Solve()
+
+    # Score.
+    total_count = sum(v for v in row_sums)
+    overlap = 0
+    for arc in range(min_cost_flow.NumArcs()):
+        # Can ignore arcs leading out of source or into sink.
+        if min_cost_flow.Tail(arc)!=source and min_cost_flow.Head(arc)!=sink:
+            # Arcs in the solution have a flow value of 1. Their start and end nodes
+            # give an assignment of worker to task.
+            if min_cost_flow.Flow(arc) > 0:
+                row_num = min_cost_flow.Tail(arc)
+                col_num = min_cost_flow.Head(arc)
+                col = num_to_col[col_num - len(num_to_row)]
+                row = num_to_row[row_num]
+                if col in contingency[row]:
+                    overlap += contingency[row][col]
+    print("{:5.2f}   one-to-one".format(overlap * 100 / total_count))
+
+def overlap(y_true, y_pred, index_true, index_pred):
+    right = 0
+    total = 0
+    for i in range(len(y_true)):
+        if y_true[i] == index_true:
+            total += 1
+            if y_pred[i] == index_pred:
+                right += 1
+    return right, total
+
+
+def one_to_one_me(y_true, y_pred):
+    total_num = len(y_true)
+    cluster_num = max(y_true)
+    truth_list = [i for i in range(cluster_num)]
+    pred_list = [i for i in range(max(y_pred))]
+    total_right = 0
+    for i in truth_list:
+        max_right_i = 0
+        j_index = 0
+        for j in pred_list:
+            right, total = overlap(y_true, y_pred, i, j)
+            if right >= max_right_i:
+                max_right_i = right
+                j_index = j
+        total_right += total
+        pred_list.pop(pred_list.index(j_index))
+        print(total_right, total_num)
+        print("{:5.2f} one-to-one".format(total_right * 100 / total_num))
+    return total_right/total_num
+        
+
+
+def compare(predicted_labels, truth_labels, metric):
+    if metric == 'purity':
+        purity_scores = []
+        for y_true, y_pred in zip(truth_labels, predicted_labels):
+            purity_scores.append(calculate_purity_scores(y_true, y_pred))
+        return sum(purity_scores)/len(purity_scores)
+    elif metric == 'NMI':
+        NMI_scores = []
+        for y_true, y_pred in zip(truth_labels, predicted_labels):
+            NMI_scores.append(normalized_mutual_info_score(y_true, y_pred, average_method='arithmetic'))
+        return sum(NMI_scores)/len(NMI_scores)
+    elif metric == 'ARI':
+        ARI_scores = []
+        for y_true, y_pred in zip(truth_labels, predicted_labels):
+            ARI_scores.append(metrics.adjusted_rand_score(y_true, y_pred))
+        return sum(ARI_scores)/len(ARI_scores)
+    elif metric == "shen_f":
+        f_scores = []
+        for y_true, y_pred in zip(truth_labels, predicted_labels):
+            f_scores.append(calculate_shen_f_score(y_true, y_pred))
+        return sum(f_scores)/len(f_scores)
+    elif metric == "Loc3":
+        loc_scores = []
+        for y_true, y_pred in zip(truth_labels, predicted_labels):
+            loc_scores.append(elsner_local_error(y_true, y_pred))
+        return sum(loc_scores)/len(loc_scores)
+    elif metric == "one2one":
+        loc_scores = []
+        for y_true, y_pred in zip(truth_labels, predicted_labels):
+            loc_scores.append(one_to_one_me(y_true, y_pred))
+        return sum(loc_scores)/len(loc_scores)
+
+
